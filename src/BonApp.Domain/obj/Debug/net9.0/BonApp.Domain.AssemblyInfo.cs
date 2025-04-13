6fdeb498b5933c6dafe4a16a52f44dc57da9d5a3
Camlnguyet <nhatnguyet.cam.freelancer@gmail.com>
2025-04-13 22:21:55 +0700

    Update

--- src/BonApp.Domain/obj/Debug/net9.0/BonApp.Domain.AssemblyInfo.cs
+++ src/BonApp.Domain/obj/Debug/net9.0/BonApp.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BonApp.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ab1b536af21242abd3af8d8340ba5adc39f2611")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c39042a885543962fe6e1e714fd1abcabed576f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BonApp.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BonApp.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
